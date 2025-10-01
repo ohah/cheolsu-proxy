ae71bf816097849ef9db9e80b129f0cad6c50cda
wontaezia <wontae1206@gmail.com>
2025-10-01 17:34:53 +0900

    feat: add host path tree widget for network visualization

--- tauri-ui/src/pages/network-dashboard/ui/network-dashboard.tsx
+++ tauri-ui/src/pages/network-dashboard/ui/network-dashboard.tsx
@@ -9,6 +9,7 @@ import { NetworkTable } from '@/widgets/network-table';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/shared/ui';
 
 import { useProxyEventControl, useProxyInitialization, useTransactionFilters, useTransactions } from '../hooks';
+import { HostPathTree } from '@/widgets/host-path-tree/ui/host-path-tree';
 
 export const NetworkDashboard = () => {
   const { isConnected } = useProxyInitialization();
@@ -67,7 +68,17 @@ export const NetworkDashboard = () => {
           direction="horizontal"
           className="flex-1 flex border border-b-0 rounded-tl-lg shadow-[0_0_10px_0_rgba(0,0,0,0.05)] bg-background"
         >
-          <ResizablePanel className="flex-1 h-full overflow-hidden">
+          <ResizablePanel className="h-full overflow-hidden" maxSize={40} minSize={20} defaultSize={25}>
+            <HostPathTree
+              transactions={filteredTransactions}
+              selectedTransaction={selectedTransaction}
+              createTransactionSelectHandler={createTransactionSelectHandler}
+            />
+          </ResizablePanel>
+
+          <ResizableHandle withHandle />
+
+          <ResizablePanel defaultSize={75} className="flex flex-1 h-full overflow-hidden">
             <NetworkTable
               transactions={filteredTransactions}
               selectedTransaction={selectedTransaction}
@@ -75,17 +86,21 @@ export const NetworkDashboard = () => {
               createTransactionDeleteHandler={createTransactionDeleteHandler}
             />
           </ResizablePanel>
-          {selectedTransaction && (
-            <>
-              <ResizableHandle withHandle />
-              <ResizablePanel className="w-96 h-full overflow-y-auto">
+          <ResizableHandle withHandle />
+          <ResizablePanel
+            maxSize={selectedTransaction ? 50 : 0}
+            minSize={selectedTransaction ? 35 : 0}
+            className="w-96 h-full overflow-y-auto"
+          >
+            {selectedTransaction && (
+              <>
                 <TransactionDetails
                   transaction={selectedTransaction}
                   clearSelectedTransaction={clearSelectedTransaction}
                 />
-              </ResizablePanel>
-            </>
-          )}
+              </>
+            )}
+          </ResizablePanel>
         </ResizablePanelGroup>
       </div>
     </div>
