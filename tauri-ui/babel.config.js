68f6ff24c24d35085dca177040d81f5379a64307
ohah <bookyoon173@gmail.com>
2025-10-15 07:21:45 +0900

    fix: Add Babel presets for TypeScript and React parsing in React Compiler setup

--- tauri-ui/babel.config.js
+++ tauri-ui/babel.config.js
@@ -3,11 +3,18 @@ const ReactCompilerConfig = {
   // 자세한 옵션은 https://react.dev/learn/react-compiler#configuration 참조
 };
 
-module.exports = function () {
+module.exports = function (api) {
+  // Babel 캐싱 설정
+  api.cache(true);
+
   return {
+    presets: [
+      ['@babel/preset-env', { targets: { node: 'current' } }],
+      ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
+      ['@babel/preset-react', { runtime: 'automatic' }],
+    ],
     plugins: [
       ['babel-plugin-react-compiler', ReactCompilerConfig], // 반드시 먼저 실행!
-      '@babel/plugin-syntax-jsx',
     ],
   };
 };
