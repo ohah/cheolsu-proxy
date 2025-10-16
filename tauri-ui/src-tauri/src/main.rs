df15edb14dc5df37e506e214fb95b2924516070c
ohah <bookyoon173@gmail.com>
2025-10-16 23:00:32 +0900

    feat: logger add

--- tauri-ui/src-tauri/src/main.rs
+++ tauri-ui/src-tauri/src/main.rs
@@ -1,6 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tracing_subscriber::fmt;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::EnvFilter;
 
 fn main() {
+    let file_appender = tracing_appender::rolling::daily("logs", "proxy.log");
+    let (non_blocking_writer, _guard) = tracing_appender::non_blocking(file_appender);
+
+    let filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+
+    tracing_subscriber::registry()
+        .with(
+            fmt::layer()
+                .with_writer(non_blocking_writer)
+                .with_ansi(false) // When writing to a file, it's best to disable ANSI color codes.
+        )
+        .with(filter)
+        .init();
     cheolsu_proxy::run()
 }
